5fdea6dd87a373002214a8e7bd22864c29d0c100
Petter Eriksson <periksson@splunk.com>
2013-08-27 17:03:31 -0700

    aliens were in the swarm

--- src/space_invaders/collisions.clj
+++ src/space_invaders/collisions.clj
@@ -14,16 +14,17 @@
     (contains x y top bottom left right)))
 
 (defn- alien-hits [game]
-  (->> (for [p (:projectiles game)
-             a (:aliens game)
-             :when (alien-contains-projectile? p a (:alien-swarm game))] 
-         [p a])
-       (reduce (fn [hits [p a]] (s/union hits #{p a})) #{})))
+  (let [alien-swarm (:alien-swarm game)]
+    (->> (for [p (:projectiles game)
+               a (:aliens alien-swarm)
+               :when (alien-contains-projectile? p a alien-swarm)]
+           [p a])
+         (reduce (fn [hits [p a]] (s/union hits #{p a})) #{}))))
 
 (defn handle-collisions [game]
   (let [hits (alien-hits game)
         missed? #(complement (contains? hits %))]
     (-> game
         (assoc :projectiles (filter missed? (:projectiles game)))
-        (assoc-in [:alien-swarm :aliens] (filter missed? (:aliens game))))))
+        (assoc-in [:alien-swarm :aliens] (filter missed? (-> game :alien-swarm :aliens))))))
 
